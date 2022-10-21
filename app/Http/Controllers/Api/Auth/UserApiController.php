14c24fede3014be93940e5fadd8b1caa2bb583a6
Dante Cuevas <danieldantecuevas@gmail.com>
2022-10-21 13:35:45 -0500

    add passport auth with resource, request and test to login

---
+++ app/Http/Controllers/Api/Auth/UserApiController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Resources\User\UserResource;
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Actions\User\LoginAction;
+
+
+class UserApiController extends Controller
+{
+
+    public function login(LoginRequest $request, LoginAction $loginAction)
+    {
+
+        try {
+
+            $response = $this->respondWithToken( $loginAction->attempt($request) );
+            return response()->json($response, 200);
+
+        } catch (Exception $e) {
+
+            return response()->json([
+                'status'    => false,
+                'message'   => $e->getMessage()
+            ], Response::HTTP_NOT_FOUND);
+
+        }
+
+    }
+
+    public function logout( Request $request )
+    {
+        try {
+
+          $request->user()->tokens->each(function($token, $key) {
+            $token->delete();
+          });
+
+          return response()->json([
+              'status'    => true,
+              'message'   => 'Session closed succesfully.'
+          ], Response::HTTP_OK);
+          
+        } catch (Exception $e) {
+
+           return response()->json([
+                'status'    => false,
+                'message'   => $e->getMessage()
+            ], Response::HTTP_NOT_FOUND);
+
+        }
+    }
+
+    public function access()
+    {
+        
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Access successfully'
+        ], 200);
+
+    }
+
+    /**
+     * Get the token array structure.
+     *
+     * @param  string $token
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function respondWithToken($token)
+    {
+        return [
+            'status'        => true,
+            'token'         => $token,
+            'token_type'    => 'Bearer',
+            'user'          => UserResource::make(auth()->user()),
+            'message'       => 'Access success',
+        ];
+    }
+}
---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+
+    private $credentials;
+    private $token;
+    
+    protected function setUp():void
+    {
+
+        parent::setUp();
+        $this->credentials = [
+            'email'     => 'danieldantecuevas@gmail.com',
+            'password'  => '12345678'
+        ];
+        $response = $this->json('POST', 'api/login', $this->credentials, ['Accept'=>'application/json']);
+        $response = json_decode($response->getContent());
+        $this->token = $response->token;
+
+    }
+
+    public function test_login_api_ok()
+    {
+        
+        $response = $this->json('POST', 'api/login', $this->credentials, ['Accept'=>'application/json']);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'status', 'token', 'token_type', 'user', 'message'
+        ]);
+
+    }
+
+    public function test_login_api_fail()
+    {
+        
+        $credentials = $this->credentials;
+        $credentials['password'] = '123456789';
+        $response = $this->json('POST', 'api/login', $credentials, ['Accept'=>'application/json']);
+        $response->assertStatus(404);
+        $response->assertJson([
+            "message"   => "Invalid credentials."
+        ]);
+
+    }
+
+    public function test_login_api_request()
+    {
+        
+        $response = $this->json('POST', 'api/login', [], ['Accept'=>'application/json']);
+        $response->assertStatus(422);
+        $response->assertJson([
+            "message"   => "The given data was invalid.",
+            "errors"    => [
+                "email"     => ["The email field is required."],
+                "password"  => ["The password field is required."]
+            ]
+        ]);
+
+        $credentials = [
+            'email'     => 'danieldantecuevas@gmail.comss',
+            'password'  => '123'
+        ];
+        $response = $this->json('POST', 'api/login', $credentials, ['Accept'=>'application/json']);
+        $response->assertStatus(422);
+        $response->assertJson([
+            "message"   => "The given data was invalid.",
+            "errors"    => [
+                "email"     => ["The selected email is invalid."],
+                "password"  => ["The password must be at least 8 characters."]
+            ]
+        ]);
+
+    }
+
+    public function test_api_access_ok()
+    {
+
+        $headers = [
+            'Accept'             => 'application/json',
+            'Authorization'     => 'Bearer '.$this->token
+        ];
+        $response = $this->withHeaders($headers)->get('api/access');
+        $response->assertStatus(200);
+        $response->assertJson([
+            'status'    => true,
+            'message'   => 'Access successfully'
+        ]);
+
+    }
+
+    public function test_api_access_fail()
+    {
+
+        $headers = [
+            'Accept'             => 'application/json',
+            'Authorization'     => 'Bearer fail'
+        ];
+        $response = $this->json('GET', 'api/access', $headers);
+        $response->assertStatus(401);
+        $response->assertJson([
+            'message'   => 'Unauthenticated.'
+        ]);
+
+    }
+
+}
